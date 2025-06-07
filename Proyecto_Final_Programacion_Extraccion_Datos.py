e95eed68d86a165fa5d693ab0317026dbf0ee05d
ArmandoRamirez951 <cardenas2200607@uabc.edu.mx>
2025-06-07 13:33:51 -0700

    Test Proyecto_Final caliz

---
+++ Proyecto_Final_Programacion_Extraccion_Datos.py
@@ -0,0 +1,9 @@
+'''
+Proyecto Final - Programacion para Extraccion de Datos
+integrantes:
+    -- De La Cruz Ramirez Jeremy Yael
+    -- Ramirez Cardenas Luis Armando
+Grupo: 951
+Fecha:
+Profesor: Josue Miguel Flores Parra
+'''
