62bad67ff39bcfd24b5eb6b95c30273cf67bd083
ArmandoRamirez951 <cardenas2200607@uabc.edu.mx>
2025-06-07 13:48:52 -0700

    Test Proyecto_Final importe necesarias para el respectivo codigo

--- Proyecto_Final_Programacion_Extraccion_Datos.py
+++ Proyecto_Final_Programacion_Extraccion_Datos.py
@@ -8,3 +8,27 @@ Fecha:
 Profesor: Josue Miguel Flores Parra
 '''
 
+import logging
+import os
+import sys
+import time
+import pandas as pd
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from bs4 import BeautifulSoup
+import mysql.connector
+from sqlalchemy import create_engine
+import dash
+import dash_bootstrap_components as dbc
+from dash import dcc, html, Input, Output
+import plotly.express as px
+from webdriver_manager.chrome import ChromeDriverManager
+from sqlalchemy.exc import OperationalError
+from mysql.connector import Error
+from tkinter import *
+from tkinter import simpledialog, messagebox
+root = Tk()
+root.withdraw()
